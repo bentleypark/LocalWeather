ae50d8b4e68d252259174ad1ae42fe7642547f0d
Bentley Park <junggil.park@sovoro.kr>
2021-01-12 20:43:35 +0900

    buildSrc 추가

---
+++ buildSrc/src/main/java/Modules.kt
@@ -0,0 +1,11 @@
+object Modules {
+    val app = ":app"
+    val data = ":data"
+    val commonUi = ":common:ui"
+    val commonUtils = ":common:utils"
+    val featureStt = ":feature-stt"
+    val featureTts = ":feature-tts"
+    val featureSplash = ":feature-splash"
+    val featureAac = ":feature-aac"
+    val resources = ":resources"
+}
