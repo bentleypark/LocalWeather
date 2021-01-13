78a7d8c83423fcfb7881f528863039959a82dfa4
Bentley Park <junggil.park@sovoro.kr>
2021-01-13 14:05:09 +0900

    RepositoryModule 수정

--- app/src/main/java/com/bentley/localweather/di/RepositoryModule.kt
+++ app/src/main/java/com/bentley/localweather/di/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.bentley.localweather.di
 
 import com.bentley.localweather.data.ApiService
 import com.bentley.localweather.data.WeatherRepositoryImpl
+import com.bentley.localweather.data.mapper.WeatherMapper
 import com.bentley.localweather.domain.WeatherRepository
 import dagger.Module
 import dagger.Provides
@@ -17,7 +18,14 @@ object RepositoryModule {
     @Provides
     fun provideMainRepository(
         apiService: ApiService,
+        weatherMapper: WeatherMapper
     ): WeatherRepository {
-        return WeatherRepositoryImpl(apiService)
+        return WeatherRepositoryImpl(apiService, weatherMapper)
+    }
+
+    @Singleton
+    @Provides
+    fun provideWeatherMapper(): WeatherMapper {
+        return WeatherMapper()
     }
 }
