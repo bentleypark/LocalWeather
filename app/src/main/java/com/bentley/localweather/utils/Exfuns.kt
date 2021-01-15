58a6edd55f5984e31f131d2e29f3c97d0b9cd23e
Bentley Park <junggil.park@sovoro.kr>
2021-01-15 11:40:30 +0900

    코드 정리

--- app/src/main/java/com/bentley/localweather/utils/Exfuns.kt
+++ app/src/main/java/com/bentley/localweather/utils/Exfuns.kt
@@ -1,10 +1,8 @@
 package com.bentley.localweather.utils
 
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
-import com.google.android.material.snackbar.Snackbar
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -14,15 +12,6 @@ fun View.makeVisible() {
 
 fun View.makeGone() {
     this.visibility = View.GONE
-}
-
-fun Fragment.makeToast(text: String) {
-    val toast = Toast.makeText(activity, text, Toast.LENGTH_SHORT)
-    toast.show()
-}
-
-fun View.makeSnackBar(text: String) {
-    Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
 }
 
 fun <T> Fragment.viewLifecycle(): ReadWriteProperty<Fragment, T> =
